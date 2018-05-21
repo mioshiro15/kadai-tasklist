df7ddf58e83917873419fae65bc2629adda7a62c
kadai-tasklist <ec2-user@ip-172-31-35-181.us-east-2.compute.internal>
2018-05-21 07:23:04 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -33,7 +33,13 @@ class TasksController extends Controller
     // postでmessages/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'title' => 'required|max:191',   // 追加
+            'content' => 'required|max:191',
+        ]);
+        
         $task = new Task;
+        $task->title = $request->title;      // 追加
         $task->content = $request->content;
         $task->save();
 
@@ -63,7 +69,13 @@ class TasksController extends Controller
     // putまたはpatchでmessages/idにアクセスされた場合の「更新処理」
     public function update(Request $request,$id)
     {
+        $this->validate($request, [
+            'title' => 'required|max:191',   // 追加
+            'content' => 'required|max:191',
+        ]);
+        
         $task = Task::find($id);
+        $task->title = $request->title;    // 追加
         $task->content = $request->content;
         $task->save();
 
