fc86e8a4900cd648e38b7a6773e5dee0400db8e0
kadai-tasklist <ec2-user@ip-172-31-35-181.us-east-2.compute.internal>
2018-05-22 09:26:32 +0000

    kadai-tasklist

--- database/migrations/2018_05_21_054403_add_title_to_tasks_table.php
+++ database/migrations/2018_05_21_054403_add_title_to_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTitleToTasksTable extends Migration
+class AyuhshdheudhTable extends Migration
 {
     /**
      * Run the migrations.
