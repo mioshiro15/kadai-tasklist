4f9253c0c2a3b05c93a7b1f0e52731e15024bb8b
kadai-tasklist <ec2-user@ip-172-31-35-181.us-east-2.compute.internal>
2018-05-23 01:46:04 +0000

    bootstrap design

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,6 +4,12 @@
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
+ <div class="row">
+        <div class="col-xs-12"></div>
+        <div class="col-sm-offset-2 col-sm-6"></div>
+        <div class="col-md-offset-2 col-sm-6"></div>
+        <div class="col-lg-offset-3 col-lg-3"></div>
+
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
         {!! Form::label('content', 'タスク:') !!}
@@ -15,5 +21,8 @@
         {!! Form::submit('更新') !!}
 
     {!! Form::close() !!}<!-- ここにページ毎のコンテンツを書く -->
+
+</div>
+    </div>
 
 @endsection
